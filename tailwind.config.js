53eacaa7bdca251c63b212b66dc89b0638397899
Vanshikatiwari-0212 <vanshikat225@gmail.com>
2025-01-12 21:05:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+     
+      "coffee",
+      
+    ],
+  },
+};
